06b68beeeecc8691fdea50da9d82ac12e4791fb1
unknown <sahani_a@EYEPAXWS-72.3logixs.lk>
2015-08-31 17:21:02 +0530

    change css styles add js

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,3 @@
-/* =================================
-   LOADER                     
-=================================== */
-// makes sure the whole site is loaded
-jQuery(window).load(function() {
-        // will first fade out the loading animation
-	jQuery(".status").fadeOut();
-        // will fade out the whole DIV that covers the website.
-	jQuery(".preloader").delay(1000).fadeOut("slow");
-})
 
 /* =================================
 ===  RESPONSIVE VIDEO           ====
@@ -15,26 +5,6 @@ jQuery(window).load(function() {
 
 $(".video-container").fitVids();
 
-
-
-/* =================================
-===  MAILCHIMP                 ====
-=================================== */
-
-$('.mailchimp').ajaxChimp({
-    callback: mailchimpCallback,
-    url: "http://webdesign7.us6.list-manage.com/subscribe/post?u=9445a2e155b82208d73433060&amp;id=16dc80e353" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
-});
-
-function mailchimpCallback(resp) {
-     if (resp.result === 'success') {
-        $('.subscription-success').html('<i class="icon_check_alt2"></i><br/>' + resp.msg).fadeIn(1000);
-        $('.subscription-error').fadeOut(500);
-        
-    } else if(resp.result === 'error') {
-        $('.subscription-error').html('<i class="icon_close_alt2"></i><br/>' + resp.msg).fadeIn(1000);
-    }  
-}
 
 /* =================================
 ===  STICKY NAV                 ====
@@ -89,29 +59,21 @@ if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
 
 
 /* =================================
-===  DOWNLOAD BUTTON CLICK SCROLL ==
+===     Event HIDE/SHOW        ====
 =================================== */
-jQuery(function( $ ){
-			$('#download-button').localScroll({
-				duration:1000
-			});
-		});
 
-
-/* =================================
-===  FULL SCREEN HEADER         ====
-=================================== */
-function alturaMaxima() {
-  var altura = $(window).height();
-  $(".full-screen").css('min-height',altura); 
-  
-}
-
-$(document).ready(function() {
-  alturaMaxima();
-  $(window).bind('resize', alturaMaxima);
-});
-
+	
+function showonlyone(thechosenone) {
+    $('.event-details').each(function(index) {
+        if ($(this).attr("id") == thechosenone) {
+            $(this).show();
+                }
+          else {
+            $(this).hide();
+          }
+        });
+       }
+		
 
 /* =================================
 ===  SMOOTH SCROLL             ====
@@ -200,75 +162,6 @@ $(document).ready(function () {
     });
 
 });
-
-
-/* =================================
-===  SUBSCRIPTION FORM          ====
-=================================== */
-$("#subscribe").submit(function (e) {
-    e.preventDefault();
-    var email = $("#subscriber-email").val();
-    var dataString = 'email=' + email;
-
-    function isValidEmail(emailAddress) {
-        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-        return pattern.test(emailAddress);
-    };
-
-    if (isValidEmail(email)) {
-        $.ajax({
-            type: "POST",
-            url: "subscribe/subscribe.php",
-            data: dataString,
-            success: function () {
-                $('.subscription-success').fadeIn(1000);
-                $('.subscription-error').fadeOut(500);
-                $('.hide-after').fadeOut(500);
-            }
-        });
-    } else {
-        $('.subscription-error').fadeIn(1000);
-    }
-
-    return false;
-});
-
-
-
-/* =================================
-===  CONTACT FORM          ====
-=================================== */
-$("#contact").submit(function (e) {
-    e.preventDefault();
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var subject = $("#subject").val();
-    var message = $("#message").val();
-    var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
-
-    function isValidEmail(emailAddress) {
-        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-        return pattern.test(emailAddress);
-    };
-
-    if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-        $.ajax({
-            type: "POST",
-            url: "sendmail.php",
-            data: dataString,
-            success: function () {
-                $('.success').fadeIn(1000);
-                $('.error').fadeOut(500);
-            }
-        });
-    } else {
-        $('.error').fadeIn(1000);
-        $('.success').fadeOut(500);
-    }
-
-    return false;
-});
-
 
 
 /* =================================
